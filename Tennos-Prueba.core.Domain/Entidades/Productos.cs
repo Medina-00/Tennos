9425044b4bf34d4a6ce5b0e9923b5893ed6b37a1
Luis Angel <20221063@itla.edu.do>
2025-02-26 10:27:15 -0400

    CREACION DE LA ENTIDAD

---
+++ Tennos-Prueba.core.Domain/Entidades/Productos.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Tennos_Prueba.core.Domain.Entidades
+{
+    public class Productos
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    }
+}
